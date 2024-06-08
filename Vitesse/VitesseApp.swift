52c9a94497dd888f4766a851d6360f8b63c3f09a
Yannick Leplard <leplard.yannick@gmail.com>
2024-06-08 17:47:28 +0200

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Yannick LEPLARD on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
